9f8284536f05560bbd37e78668feaf50e83f31fc
Shahariar Kabir <maask5534@gmail.com>
2023-02-22 20:31:34 +0600

    Completed BL026

--- entry.js
+++ entry.js
@@ -1,17 +1,1 @@
-const { join } = require("path");
-const base = join(__dirname, "src/clients/brainlab/baliblinds/BL027");
-module.exports = {
-  js: join(base, "index.ts"),
-  css: join(base, "styles/main.scss"),
-};
-module.exports.getRunningTestInfo = () => {
-  console.log(
-    "Running test info: ",
-    JSON.stringify({
-      id: "BL027",
-      site: "baliblinds",
-      client: "brainlab",
-      variation: "1",
-    })
-  );
-};
+const {join} = require("path");const base = join(__dirname, "src/clients/brainlab/graberblinds/BL026");module.exports={js:join(base,"index.ts"),css:join(base,"styles/main.scss")};module.exports.getRunningTestInfo=()=>{console.log("Running test info: ",JSON.stringify({id:'BL026',site:'graberblinds',client:'brainlab',variation: '1'}))};
