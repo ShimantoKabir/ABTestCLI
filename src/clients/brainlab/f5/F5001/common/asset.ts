1264b77fad04f12f09cb9efb6fe3198c00b8f6c6
Shahariar Kabir <maask5534@gmail.com>
2023-04-19 03:27:03 +0600

    Completed 80% of F5001

---
+++ src/clients/brainlab/f5/F5001/common/asset.ts
@@ -0,0 +1,129 @@
+import { CartModel } from "../models/cart.model";
+
+export const facebookSvgLink: string =
+  "https://www.f5.com/etc.clientlibs/base/clientlibs/base-resources/resources/icons/social/facebook-grayscale.svg";
+export const twitterSvgLink: string =
+  "https://www.f5.com/etc.clientlibs/base/clientlibs/base-resources/resources/icons/social/twitter-grayscale.svg";
+export const linkedinSvgLink: string =
+  "https://www.f5.com/etc.clientlibs/base/clientlibs/base-resources/resources/icons/social/linkedin-grayscale.svg";
+export const shareSvgLink: string =
+  "https://www.f5.com/etc.clientlibs/base/clientlibs/base-resources/resources/icons/social/addthis_share.svg";
+export const publisherImgLink: string =
+  "https://www.f5.com/authors/kara-sprague.thumb.png";
+export const publishedDate = "APRIL 06, 2023";
+export const publisherName = "KARA SPRAGUE";
+export const publisherLink = "https://www.f5.com/authors/kara-sprague";
+
+export const blogs: CartModel[] = [
+  {
+    href: "https://www.f5.com/company/blog/secure-multi-cloud-networking",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/related-blogs/1_blog-multicloud-illustration.png",
+    linkText: "Hybrid, Multi-cloud - Why F5 Invests",
+    title: "BLOG",
+  },
+  {
+    href: "https://www.f5.com/company/blog/mastering-api-architecture-api-fundamentals",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/related-blogs/2_blog-mastering-api-fundamentals.png",
+    linkText: "Mastering API Architecture: API Fundamentals",
+    title: "BLOG",
+  },
+  {
+    href: "https://www.f5.com/company/blog/taking-bad-bots-head-on-with-f5-and-aws",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/related-blogs/3_taking-bad-bots-head-on.png",
+    linkText: "Taking Bad Bots Head On with F5 and AWS",
+    title: "BLOG",
+  },
+  {
+    href: "https://www.f5.com/company/blog/mitigating-architectural-risk",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/related-blogs/4_mitigating-architectural-risk.png",
+    linkText: "Mitigating Architectural<br> Risk",
+    title: "BLOG",
+  },
+  {
+    href: "https://www.f5.com/company/blog/f5-distributed-cloud-services-stands-up-to-l7-ddos-attacks",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/related-blogs/5_xc-stands-up-to-emerging-ddos-attacks.png",
+    linkText:
+      "F5 Distributed Cloud Services Stands Up to Emerging L7 DDoS Attacks",
+    title: "BLOG",
+  },
+];
+
+export const securities: CartModel[] = [
+  {
+    href: "https://www.f5.com/solutions/use-cases/multi-cloud-networking",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/additional-resources/1_multi-cloud-networking.png",
+    linkText: "Build Anywhere with Comprehensive Multi-Cloud Networking",
+    title: "Use Case",
+  },
+  {
+    href: "https://www.f5.com/resources/reports/state-of-application-strategy-report",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/additional-resources/2_soas-2023-illo-option-cover.png",
+    linkText: "2023 State of Application Strategy<br> Report",
+    title: "Report",
+  },
+  {
+    href: "https://www.f5.com/solutions/forrester-report-api-security-threats-and-solutions-b",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/additional-resources/3_F5-apis-forrester-report-gated.png",
+    linkText: "API Security in the hybrid multi cloud<br> world",
+    title: "Forester Report",
+  },
+];
+
+export const aboutLinks: CartModel[] = [
+  {
+    href: "https://www.f5.com/solutions/waap-buyers-guide-ebook",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/additional-resources/4_waap-buying-guide.png",
+    linkText: "WAAP Buying Guide: Security as a Digital Differentiator",
+    title: "eBook",
+  },
+  {
+    href: "https://community.f5.com/t5/technical-articles/f5-hybrid-security-architectures-one-waf-engine-total/ta-p/307248",
+    imgSrc:
+      "https://medianet.com/cro/f5/bl029/additional-resources/5_hybrid-security-architectures-one-waf-engine.png",
+    linkText:
+      "F5 Hybrid Security Architectures: One WAF Engine, Total Flexibility",
+    title: "Article",
+  },
+  {
+    href: "#dummy",
+    imgSrc:
+      "https://www.f5.com/content/dam/f5-com/global-assets/resources-featurettes/dist-cloud-blog-feature_730x508.png",
+    linkText: "F5 Announces General Availability of NGINXaaS for Azure",
+    title: "BLOG",
+  },
+];
+
+export const rightArrow = `
+<svg width="15" height="15" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M15.9766 8.99805L0.976562 8.99805" stroke="#0E6EB9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M8.80859 16.498L15.9767 8.99805L8.80859 1.49805" stroke="#0E6EB9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
+`;
+
+export const socialPopUp = {
+  facebook: {
+    url: "https://www.addthis.com/bookmark.php?v=300&winname=addthis&pub=ra-5095d30f38626622&source=tbx-300,men-300&lng=en&s=facebook&url=https%3A%2F%2Fwww.f5.com%2Fcompany%2Fblog%2Fwhat-hybrid-it-means-for-app-and-api-security&title=What%20Hybrid%20IT%20Means%20for%20App%20and%20API%20Security&ate=AT-ra-5095d30f38626622/-/-/642d84ff65edd1ce/3&frommenu=1&uid=642d84ffbde33de1&description=The%20year%202022%20will%20be%20remembered%20as%20the%20year%20the%20industry%20finally%20reconciled%20itself%20to%20the%20fact%20that%20IT%20is%20and%20will%20be%20for%20the%20foreseeable%20future%2C%20hybrid.%20The%20question%20is%2C%20what%20does%20that%20mean%20for%20security%20and%2C%20specifically%2C%20for%20app%20and%20API%20security.&uud=1&ct=1&tt=0&captcha_provider=recaptcha2&pro=1",
+    name: "Facebook",
+  },
+  twitter: {
+    url: "https://www.addthis.com/bookmark.php?v=300&winname=addthis&pub=ra-5095d30f38626622&source=tbx-300,men-300&lng=en&s=twitter&url=https%3A%2F%2Fwww.f5.com%2Fcompany%2Fblog%2Fwhat-hybrid-it-means-for-app-and-api-security&title=What%20Hybrid%20IT%20Means%20for%20App%20and%20API%20Security&ate=AT-ra-5095d30f38626622/-/-/642d84ff65edd1ce/4&frommenu=1&uid=642d84fffd4eed68&description=The%20year%202022%20will%20be%20remembered%20as%20the%20year%20the%20industry%20finally%20reconciled%20itself%20to%20the%20fact%20that%20IT%20is%20and%20will%20be%20for%20the%20foreseeable%20future%2C%20hybrid.%20The%20question%20is%2C%20what%20does%20that%20mean%20for%20security%20and%2C%20specifically%2C%20for%20app%20and%20API%20security.&uud=1&ct=1&tt=0&captcha_provider=recaptcha2&pro=1",
+    name: "Twitter",
+  },
+  linkedin: {
+    url: "https://www.addthis.com/bookmark.php?v=300&winname=addthis&pub=ra-5095d30f38626622&source=tbx-300,men-300&lng=en&s=linkedin&url=https%3A%2F%2Fwww.f5.com%2Fcompany%2Fblog%2Fwhat-hybrid-it-means-for-app-and-api-security&title=What%20Hybrid%20IT%20Means%20for%20App%20and%20API%20Security&ate=AT-ra-5095d30f38626622/-/-/642d84ff65edd1ce/5&frommenu=1&uid=642d84ffcb242e09&description=The%20year%202022%20will%20be%20remembered%20as%20the%20year%20the%20industry%20finally%20reconciled%20itself%20to%20the%20fact%20that%20IT%20is%20and%20will%20be%20for%20the%20foreseeable%20future%2C%20hybrid.%20The%20question%20is%2C%20what%20does%20that%20mean%20for%20security%20and%2C%20specifically%2C%20for%20app%20and%20API%20security.&uud=1&ct=1&tt=0&captcha_provider=recaptcha2&pro=1",
+    name: "Linkedin",
+  },
+};
+
+export const selectors = {
+  hiddenSectionSelector: "div#platter396527510",
+};
