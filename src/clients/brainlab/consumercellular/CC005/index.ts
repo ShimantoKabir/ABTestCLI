9ad7a83297184775df567ca66feebcfe02a9f339
Shahariar Kabir <maask5534@gmail.com>
2023-08-15 10:28:09 +0600

    working on CC005

--- src/clients/brainlab/consumercellular/CC005/index.ts
+++ src/clients/brainlab/consumercellular/CC005/index.ts
@@ -8,6 +8,13 @@ const ieChecks = /MSIE|Trident|Edge\/(12|13|14|15|16|17|18)/.test(
 
 const priceModuleSelector: string =
   "div#header-prototype-app div.shopping-due-today-display-wrapper";
+const placeOrderButtonSelector: string = "div.hide-desktop-up>div>button";
+
+const getBottomPriceModule = (): null | HTMLDivElement => {
+  return document.querySelector(
+    "div.submit-section>div.shopping-due-today-display-wrapper"
+  );
+};
 
 const runTest = () => {
   const priceModule: null | HTMLDivElement =
@@ -24,10 +31,49 @@ const runTest = () => {
         : priceModule.classList.add("hide-component");
     }
   });
+
+  const placeOrderButton: null | HTMLButtonElement = document.querySelector(
+    placeOrderButtonSelector
+  );
+
+  if (priceModule && placeOrderButton) {
+    const priceModuleClone = priceModule.cloneNode(true) as HTMLDivElement;
+
+    priceModuleClone.classList.remove("hide-component");
+
+    if (!getBottomPriceModule()) {
+      placeOrderButton.insertAdjacentElement(
+        "beforebegin",
+        priceModuleClone as Element
+      );
+    }
+  }
+
+  if (placeOrderButton) {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+
+    const callback: IntersectionObserverCallback = (
+      entries: IntersectionObserverEntry[],
+      observer: IntersectionObserver
+    ) => {
+      entries.forEach((entry: IntersectionObserverEntry) => {
+        console.log("entry=", entry);
+        if (entry.isIntersecting) {
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(placeOrderButton as Element);
+  }
 };
 
 if (!ieChecks) {
   Initializer.init(TestInfo, "0.0.1");
   const poller = new Poller();
-  poller.poll(["body", priceModuleSelector], runTest);
+  poller.poll(["body", priceModuleSelector, placeOrderButtonSelector], runTest);
 }
