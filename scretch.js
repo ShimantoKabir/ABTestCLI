3ea32c971b30a4f0ee9ff542ad2a522f5dddb241
Shahariar Kabir <maask5534@gmail.com>
2023-09-15 21:32:00 +0600

    completed F5006

--- scretch.js
+++ scretch.js
@@ -1,18 +1,21 @@
 function trigger(activate, options) {
   var utils = window.optimizely.get("utils");
+  var desktopHoverElm = "nav.main-nav>ul>li.primary-item-window-treatments";
+  var mobileHoverElm = "button.nav-mobile";
 
-  if (
-    window.location.href.indexOf(
-      "https://www.graberblinds.com/find-an-expert"
-    ) !== -1
-  ) {
-    utils
-      .waitForElement("button.o-form__submit")
-      .then(function (buttonElement) {
-        buttonElement.addEventListener("click", function () {
-          options.isActive && activate({ isActive: false });
-          activate();
-        });
+  if (window.location.href.indexOf("https://www.graberblinds.com") !== -1) {
+    utils.waitForElement(desktopHoverElm).then((desktopHoverElm) => {
+      desktopHoverElm.addEventListener("mouseenter", function () {
+        options.isActive && activate({ isActive: false });
+        activate();
       });
+    });
+
+    utils.waitForElement(mobileHoverElm).then((mobileHoverElm) => {
+      mobileHoverElm.addEventListener("click", function () {
+        options.isActive && activate({ isActive: false });
+        activate();
+      });
+    });
   }
 }
