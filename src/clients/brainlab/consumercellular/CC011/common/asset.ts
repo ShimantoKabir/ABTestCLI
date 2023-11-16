16d25edaa5e29223d16d8192b82cf7ea78f7db70
Shahariar Kabir <maask5534@gmail.com>
2023-11-16 15:34:12 +0600

    working on CC011

---
+++ src/clients/brainlab/consumercellular/CC011/common/asset.ts
@@ -0,0 +1,4 @@
+export const selectors = {
+  shoppingPageContainer: "div#shopping-page-container",
+  slickSlider: "section.most-plans>div:nth-child(3) div.slick-slider",
+};
---
+++ src/clients/brainlab/consumercellular/CC011/components/main.component.ts
@@ -0,0 +1,57 @@
+import { Initializer } from "../../../../../utilities/initializer";
+import { selectors } from "../common/asset";
+import { TestInfo } from "../common/test.info";
+import { LocationObserver } from "../observer/location.observer";
+import { TestObserver } from "../observer/test.observer";
+
+export class MainComponent {
+  location: string = "";
+  isDesiredElementFound: boolean = false;
+  planPagePathname: string = "/shopping/choose/plan";
+
+  constructor() {
+    Initializer.init(TestInfo, "0.0.1");
+  }
+
+  init = (): void => {
+    LocationObserver.listen((location: string) => {
+      this.location = location;
+      this.isDesiredElementFound = false;
+    });
+
+    const testObserver = new TestObserver(selectors.shoppingPageContainer);
+    const callback = (mutationList: MutationRecord[]) => {
+      for (let index = 0; index < mutationList.length; index++) {
+        const mutationRecord = mutationList[index];
+
+        const target: Element = mutationRecord.target as Element;
+
+        console.log("target=", target);
+
+        if (
+          this.location === this.planPagePathname &&
+          !this.isDesiredElementFound &&
+          target.classList.contains("combo-plan")
+        ) {
+          this.displaySlider();
+          this.isDesiredElementFound = true;
+          break;
+        }
+      }
+    };
+    testObserver.observe(callback);
+  };
+
+  displaySlider = () => {
+    const slickSlider: Element | null = document.querySelector(
+      selectors.slickSlider
+    );
+    console.log("slickSlider=", slickSlider);
+    if (!slickSlider) {
+      return;
+    }
+
+    console.log("first-button=", slickSlider.childNodes[1]);
+    console.log("first-button=", slickSlider.childNodes[2]);
+  };
+}
